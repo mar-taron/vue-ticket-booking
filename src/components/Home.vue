df5557b7fd161158eb9fc5059e1b27e5a7a9b64a
mar-taron <mtaron91@gmail.com>
2018-06-24 14:55:38 +0400

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content">
+    <h1>Movie Ticket Booking Widget</h1>
+    <TicketBooking />
+  </div>
+</template>
+
+<script>
+import TicketBooking from './TicketBooking';
+
+export default {
+  name: 'Home' ,
+  components: { TicketBooking }
+}
+</script>
+<style>
+.content{
+  padding:50px 0;
+}
+.content h1{
+    color: #fff;
+    font-size: 38px;
+    text-align: center;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+}
+</style>
